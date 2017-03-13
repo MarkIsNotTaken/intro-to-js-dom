c1767339161fd24478b4646f814d25c46e51e0a7
MarkIsNotTaken <mdemesillo@gmail.com>
2017-03-13 08:34:54 -1000

    .

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("Sanity Check");
+//finding HTML elements
+//console.log(document.getElementById("use-id-method"));
+var idElement = document.getElementById("use-id-method");
+console.log(idElement);
+//console.log(document.getElementsByTagName("div"));
+var tagElement = document.getElementsByTagName("div");
+console.log(tagElement);
+//console.log(document.getElementsByClassName("use-class-method"));
+var classElement = document.getElementsByClassName("use-class-method");
+console.log(classElement);
